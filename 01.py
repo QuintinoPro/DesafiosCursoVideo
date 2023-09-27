b709ae126a5424a6a99fbf859053984a86dba77f
Lucas Quintino Gomes Araujo <lucas.quintino@bndes.gov.br>
2023-09-27 14:44:17 -0300

    First Commit

---
+++ 01.py
@@ -0,0 +1,50 @@
+
+n1 = int(input("\nDigite um numero: "))
+
+print("\n------Desafio 05")
+print(f'Sucessor: {n1 + 1}')
+print(f'Antecessor: {n1 - 1}')
+
+print("\n------Desafio 06")
+print(f'Dobro: {n1*2}')
+print(f'Raiz Quadrada: {n1**2}')
+
+print("\n------Desafio 07")
+nota1 = float(input("Nota 1: "))
+nota2 = float(input("Nota 2: "))
+
+print(f'Media das Notas: {(nota1+nota2)/2}')
+
+print("\n------Desafio 08")
+metros = float(input("Digite os Metros: "))
+
+print(f'Centimetros: {metros * 100}')
+print(f'Milimetros: {metros * 1000}')
+
+print("\n------Desafio 09")
+for x in range(1,10):
+    print(f'{n1} * {x} = {n1 * x}')
+
+print("\n------Desafio 10")
+salr = float(input("Saldo em Reais: "))
+print(f'Saldo em Dolares: {salr/4.94:.2f}')
+
+print("\n------Desafio 11")
+larg = float(input("Largura: "))
+alt = float(input("Altura: "))
+area = larg * alt
+litro = 2
+litros = area/2
+print(f'Precisa de {litros} para pintar')
+
+print("\n------Desafio 12")
+preco = float(input("Preco: "))
+print(f'O novo preco é {preco - (preco * 0.05):.2f}')
+
+print("\n------Desafio 13")
+sala = float(input("Digite seu Salario: "))
+print(f'Salario com 15% de Aumento {sala + (sala * 0.15):.2f}')
+
+
+
+
